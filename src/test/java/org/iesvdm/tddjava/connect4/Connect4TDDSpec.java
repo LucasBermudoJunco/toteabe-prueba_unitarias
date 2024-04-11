243fd180a3a16269b86a704f25d82cd617ad0500
LucasBermudo <lberjun898@g.educaand.es>
2024-04-11 11:12:28 +0200

    tests 6, 7, 8, 11, 13 y 14

--- src/test/java/org/iesvdm/tddjava/connect4/Connect4TDDSpec.java
+++ src/test/java/org/iesvdm/tddjava/connect4/Connect4TDDSpec.java
@@ -42,7 +42,8 @@ public class Connect4TDDSpec {
 
             // WHEN & DO
 
-            // Reinicialización del objeto Connect4TDD en cada iteración
+            // Reinicialización del objeto Connect4TDD en cada iteración mayor a la primera
+            // para volver a tener el juego recién empezado
             if(h>0){
                 tested = new Connect4TDD(new PrintStream(output));
             }
@@ -74,7 +75,7 @@ public class Connect4TDDSpec {
 
             // WHEN
 
-            // Reinicialización del objeto Connect4TDD en cada iteración
+            // Reinicialización del objeto Connect4TDD en cada iteración mayor a la primera
             // para que sus columnas estén vacías
             if(h>0){
                 tested = new Connect4TDD(new PrintStream(output));
@@ -85,19 +86,31 @@ public class Connect4TDDSpec {
 
             // Elección de una columna al azar dentro de las columnas posibles
             int cantidadDeColumnasDelJuego = 7;
-            int columnaInsertada = (int)(Math.random()*cantidadDeColumnasDelJuego);
+            int columnaInsertadaDentroDeLimites = (int)(Math.random()*cantidadDeColumnasDelJuego);
 
             int cantidadDeFilasDelJuego = 6;
 
+            // Llenado de esa columna
             for(int i=0; i<cantidadDeFilasDelJuego; i++){
-                tested.putDiscInColumn(columnaInsertada);
+                tested.putDiscInColumn(columnaInsertadaDentroDeLimites);
             }
+
+            // Elección de una columna al azar fuera de las columnas posibles
+            int columnaInsertadaFueraLimites;
+            do{
+                columnaInsertadaFueraLimites = (int)(Math.random()*201-100);
+            } while(columnaInsertadaFueraLimites >=0 && columnaInsertadaFueraLimites<cantidadDeColumnasDelJuego);
 
             // DO & THEN
 
             // Comprobación de que se lanza una RuntimeException cuando intentamos
             // poner un disco en una columna llena
-            Assertions.assertThrows(RuntimeException.class, () -> tested.putDiscInColumn(columnaInsertada));
+            assertThatThrownBy(() -> tested.putDiscInColumn(columnaInsertadaDentroDeLimites)).isInstanceOf(RuntimeException.class);
+
+            // Comprobación de que se lanza una RuntimeException cuando intentamos
+            // poner un disco en una columna que no existe
+            int finalColumnaInsertadaFueraLimites = columnaInsertadaFueraLimites;
+            assertThatThrownBy(() -> tested.putDiscInColumn(finalColumnaInsertadaFueraLimites)).isInstanceOf(RuntimeException.class);
 
         }
 
@@ -114,7 +127,7 @@ public class Connect4TDDSpec {
 
             // WHEN
 
-            // Reinicialización del objeto Connect4TDD en cada iteración
+            // Reinicialización del objeto Connect4TDD en cada iteración mayor a la primera
             // para que sus columnas estén vacías
             if(h>0){
                 tested = new Connect4TDD(new PrintStream(output));
@@ -143,7 +156,7 @@ public class Connect4TDDSpec {
 
             // WHEN
 
-            // Reinicialización del objeto Connect4TDD en cada iteración
+            // Reinicialización del objeto Connect4TDD en cada iteración mayor a la primera
             // para que sus columnas estén vacías
             if(h>0){
                 tested = new Connect4TDD(new PrintStream(output));
@@ -180,7 +193,7 @@ public class Connect4TDDSpec {
 
             // WHEN
 
-            // Reinicialización del objeto Connect4TDD en cada iteración
+            // Reinicialización del objeto Connect4TDD en cada iteración mayor a la primera
             if(h>0){
                 tested = new Connect4TDD(new PrintStream(output));
                 numeroDeDiscosAnteriores = tested.getNumberOfDiscs();
@@ -208,11 +221,62 @@ public class Connect4TDDSpec {
 
     }
 
+    /**
+     * Este test es igual que el ´´whenDiscOutsideBoardThenRuntimeException`` pero solamente usamos la parte
+     * de las columnas desbordadas y no de las columnas que no existen
+     */
     @Test
     public void whenNoMoreRoomInColumnThenRuntimeException() {
 
+        int cantComprob = 100;
 
+        // Comprobaciones
+        for(int h=0; h<cantComprob; h++){
 
+            // WHEN
+
+            // Reinicialización del objeto Connect4TDD en cada iteración mayor a la primera
+            // para que sus columnas estén vacías
+            if(h>0){
+                tested = new Connect4TDD(new PrintStream(output));
+            }
+
+            // Llenado de una columna para que luego podamos probar
+            // a poner un disco en dicha columna llena
+
+            // Elección de una columna al azar dentro de las columnas posibles
+            int cantidadDeColumnasDelJuego = 7;
+            int columnaInsertadaDentroDeLimites = (int)(Math.random()*cantidadDeColumnasDelJuego);
+
+            int cantidadDeFilasDelJuego = 6;
+
+            // Llenado de esa columna
+            for(int i=0; i<cantidadDeFilasDelJuego; i++){
+                tested.putDiscInColumn(columnaInsertadaDentroDeLimites);
+            }
+
+            // Inhabilitación del test de las columnas que no existen
+
+//            // Elección de una columna al azar fuera de las columnas posibles
+//            int columnaInsertadaFueraLimites;
+//            do{
+//                columnaInsertadaFueraLimites = (int)(Math.random()*201-100);
+//            } while(columnaInsertadaFueraLimites >=0 && columnaInsertadaFueraLimites<cantidadDeColumnasDelJuego);
+
+            // DO & THEN
+
+            // Comprobación de que se lanza una RuntimeException cuando intentamos
+            // poner un disco en una columna llena
+            assertThatThrownBy(() -> tested.putDiscInColumn(columnaInsertadaDentroDeLimites)).isInstanceOf(RuntimeException.class);
+
+            // Inhabilitación del test de las columnas que no existen
+
+//            // Comprobación de que se lanza una RuntimeException cuando intentamos
+//            // poner un disco en una columna que no existe
+//            int finalColumnaInsertadaFueraLimites = columnaInsertadaFueraLimites;
+//            assertThatThrownBy(() -> tested.putDiscInColumn(finalColumnaInsertadaFueraLimites)).isInstanceOf(RuntimeException.class);
+
+        }
     }
 
     /*
@@ -224,10 +288,59 @@ public class Connect4TDDSpec {
     @Test
     public void whenFirstPlayerPlaysThenDiscColorIsRed() {
 
+        int cantComprob = 100;
+
+        // Comprobaciones
+        for(int h=0; h<cantComprob; h++){
+
+            // WHEN
+
+            // Reinicialización del objeto Connect4TDD en cada iteración mayor a la primera
+            if(h>0){
+                tested = new Connect4TDD(new PrintStream(output));
+            }
+
+            // DO
+
+            String colorJugadorPrimero = tested.getCurrentPlayer();
+
+            // THEN
+
+            assertThat(colorJugadorPrimero).isEqualTo("R");
+
+        }
+
     }
 
     @Test
     public void whenSecondPlayerPlaysThenDiscColorIsGreen() {
+
+        int cantComprob = 100;
+
+        // Comprobaciones
+        for(int h=0; h<cantComprob; h++){
+
+            // WHEN
+
+            // Reinicialización del objeto Connect4TDD en cada iteración mayor a la primera
+            if(h>0){
+                tested = new Connect4TDD(new PrintStream(output));
+            }
+
+            // DO
+
+            // Tirada del primer jugador (que es el jugador rojo)
+            tested.putDiscInColumn((int)(Math.random()*7));
+
+            // Asignación al String ´´`colorJugadorSegundo` del siguiente jugador
+            // (que ahora es el segundo jugador)
+            String colorJugadorSegundo = tested.getCurrentPlayer();
+
+            // THEN
+
+            assertThat(colorJugadorSegundo).isEqualTo("G");
+
+        }
 
     }
 
@@ -255,6 +368,25 @@ public class Connect4TDDSpec {
     @Test
     public void whenTheGameStartsItIsNotFinished() {
 
+        int cantComprob = 100;
+
+        // Comprobaciones
+        for(int h=0; h<cantComprob; h++){
+
+            // WHEN & DO
+
+            // Reinicialización del objeto Connect4TDD en cada iteración mayor a la primera
+            // para volver a tener el juego recién empezado
+            if(h>0){
+                tested = new Connect4TDD(new PrintStream(output));
+            }
+
+            // THEN
+
+            assertThat(tested.isFinished()).isFalse();
+
+        }
+
     }
 
     @Test
@@ -270,6 +402,75 @@ public class Connect4TDDSpec {
     @Test
     public void when4VerticalDiscsAreConnectedThenThatPlayerWins() {
 
+        int cantComprob = 100;
+        int discosParaGanar = 4;
+
+        // Comprobaciones
+        for(int h=0; h<cantComprob; h++){
+
+            // WHEN
+
+            // Reinicialización del objeto Connect4TDD en cada iteración mayor a la primera
+            if(h>0){
+                tested = new Connect4TDD(new PrintStream(output));
+            }
+
+            // Elección al azar del jugador que queremos que gane en esta comprobación
+            String jugadorGanador="";
+            int eleccion = (int)(Math.random()*2);
+            switch(eleccion){
+                case 0: jugadorGanador = "R"; break;
+                default: jugadorGanador = "G"; break;
+            }
+
+            // Elección al azar de una columna del tablero a la que va a jugar
+            // el jugador que queremos que gane
+            int columnaDelJugadorGanador = (int)(Math.random()*7);
+
+            // Elección al azar de una columna del tablero a la que va a jugar
+            // el jugador que queremos que pierda (excluyendo de las columnas
+            // la columna a la que va a jugar el jugador que queremos que gane)
+            int columnaDelJugadorPerdedor;
+            do {
+                columnaDelJugadorPerdedor = (int) (Math.random()*7);
+            } while (columnaDelJugadorPerdedor == columnaDelJugadorGanador);
+
+            // DO
+
+            // Ajuste de la partida en caso de que el jugador que queramos que gane
+            // sea el segundo jugador (por lo que habrá que elegir una columna distinta
+            // a la columna elegida para el jugador que queramos que pierda
+            // para que así dicho primer jugador no consiga los 4 discos juntos
+            // antes que el segundo jugador)
+            if(jugadorGanador.equals("G")){
+                int columnaDeLaPrimeraTirada;
+                do{
+                    columnaDeLaPrimeraTirada = (int) (Math.random()*7);
+                } while(columnaDeLaPrimeraTirada == columnaDelJugadorPerdedor);
+
+                // Tirada del primer jugador
+                tested.putDiscInColumn(columnaDeLaPrimeraTirada);
+            }
+
+            // Jugada 7 veces (4 del jugador que queremos que gane
+            // más las jugadas de en medio del otro jugador (que serán 3 jugadas))
+            for(int i=0; i<(discosParaGanar*2)-1; i++){
+                if(i%2 == 0) {
+                    // Jugada del jugador que queremos que gane
+                    tested.putDiscInColumn(columnaDelJugadorGanador);
+                } else {
+                    // Jugada del otro jugador
+                    tested.putDiscInColumn(columnaDelJugadorPerdedor);
+
+                }
+            }
+
+            // THEN
+
+            assertThat(tested.getWinner()).isEqualTo(jugadorGanador);
+
+        }
+
     }
 
     /*
@@ -279,6 +480,74 @@ public class Connect4TDDSpec {
 
     @Test
     public void when4HorizontalDiscsAreConnectedThenThatPlayerWins() {
+
+        int cantComprob = 100;
+        int discosParaGanar = 4;
+
+        // Comprobaciones
+        for(int h=0; h<cantComprob; h++){
+
+            // WHEN
+
+            // Reinicialización del objeto Connect4TDD en cada iteración mayor a la primera
+            if(h>0){
+                tested = new Connect4TDD(new PrintStream(output));
+            }
+
+            // Elección al azar del jugador que queremos que gane en esta comprobación
+            String jugadorGanador="";
+            int eleccion = (int)(Math.random()*2);
+            switch(eleccion){
+                case 0: jugadorGanador = "R"; break;
+                default: jugadorGanador = "G"; break;
+            }
+
+            // Elección al azar de una columna del tablero a la que va a jugar
+            // el jugador que queremos que gane (dicho jugador va a jugar a esa columna
+            // y a las 3 columnas a la derecha de esta columna, por lo que la primera
+            // columna de estas 4 columnas tiene que estar entre la 0 y la 3)
+            int columnaDelJugadorGanador = (int)(Math.random()*4);
+
+            // Creación de una primera tirada en caso de que el jugador que queramos
+            // que gane sea el segundo jugador
+            int columnaDeLaPrimeraTirada = 0;
+            if(jugadorGanador.equals("G")) {
+                // Elección al azar de una columna del tablero a la que va a jugar
+                // el jugador que queremos que pierda en su primera tirada
+                // (excluyendo de las columnas las 4 columnas a las que va a jugar
+                // el jugador que queremos que gane)
+                do {
+                    columnaDeLaPrimeraTirada = (int) (Math.random() * 7);
+                } while (columnaDeLaPrimeraTirada >= columnaDelJugadorGanador &&
+                        columnaDeLaPrimeraTirada <= columnaDelJugadorGanador+3);
+            }
+
+            // DO
+
+            // Ajuste de la partida en caso de que el jugador que queramos que gane
+            // sea el segundo jugador
+            if(jugadorGanador.equals("G")){
+                // Tirada del primer jugador
+                tested.putDiscInColumn(columnaDeLaPrimeraTirada);
+            }
+
+            // Jugada 7 veces (4 del jugador que queremos que gane
+            // más las jugadas de en medio del otro jugador (que serán 3 jugadas))
+            for(int i=0; i<(discosParaGanar*2)-1; i++){
+                // El jugador perdedor va a jugar a la misma columna
+                // que el jugador ganador, pero como el jugador ganador
+                // va a conseguir la línea de 4 antes que el segundo,
+                // dicho segundo jugador no va a tener la oportunidad de ganar
+                int columnaDeEstaTirada = columnaDelJugadorGanador+(i/2);
+
+                tested.putDiscInColumn(columnaDeEstaTirada);
+            }
+
+            // THEN
+
+            assertThat(tested.getWinner()).isEqualTo(jugadorGanador);
+
+        }
 
     }
 
@@ -301,26 +570,30 @@ public class Connect4TDDSpec {
 
     // PLANTILLA DE LAS COMPROBACIONES
 
-//    // Comprobaciones
-//    int cantComprob = 100;
-//
-//        for(int h=0; h<cantComprob; h++){
-//
-//        // WHEN
-//
-//        // Reinicialización del objeto Connect4TDD en cada iteración
-//        if(h>0){
-//            tested = new Connect4TDD(new PrintStream(output));
-//        }
-//
-//        // DO
-//
-//
-//
-//        // THEN
-//
-//
-//
-//    }
+/*
+
+        int cantComprob = 100;
+
+        // Comprobaciones
+        for(int h=0; h<cantComprob; h++){
+
+            // WHEN
+
+            // Reinicialización del objeto Connect4TDD en cada iteración mayor a la primera
+            if(h>0){
+                tested = new Connect4TDD(new PrintStream(output));
+            }
+
+            // DO
+
+
+
+            // THEN
+
+
+
+        }
+
+ */
 
 }
